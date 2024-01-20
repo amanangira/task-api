d3019df6bb7297b8c03af04bb568a35653e563e7
Aman Angira <aman.angira@outlook.com>
2024-01-20 12:32:33 +0530

    Add validator helper for task controller

--- controller/task_controller.go
+++ controller/task_controller.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strings"
 	"task/app"
 	"task/app/app_error"
 	"task/persistence/models"
@@ -10,6 +12,10 @@ import (
 )
 
 // TODO - add validation for incoming requests
+
+type ITaskController interface {
+	Create(w http.ResponseWriter, r *http.Request)
+}
 
 type TaskController struct {
 	taskService service.ITaskService
@@ -25,15 +31,26 @@ func NewTaskController(
 func (p TaskController) Create(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	var input models.Task
-
-	// TODO - decode to view equivalent and run validations on it
 	decodeErr := json.NewDecoder(r.Body).Decode(&input)
 	if decodeErr != nil {
-		app_error.NewError(decodeErr, http.StatusBadRequest, "invalid input").Log().HttpError(w)
+		app.BadRequest(
+			w,
+			app_error.NewError(decodeErr, http.StatusBadRequest, "invalid input").Log().Error(),
+		)
+
 		return
 	}
 
-	err := p.taskService.Create(ctx, &input)
+	if err := validateCreatePayload(input); err != nil {
+		app.BadRequest(
+			w,
+			err.Error(),
+		)
+
+		return
+	}
+
+	id, err := p.taskService.Create(ctx, input)
 	if err != nil {
 		app_error.NewError(err, http.StatusInternalServerError, "").Log().HttpError(w)
 		return
@@ -42,6 +59,26 @@ func (p TaskController) Create(w http.ResponseWriter, r *http.Request) {
 	app.WriteJSON(w, http.StatusOK, struct {
 		ID string
 	}{
-		ID: input.ID,
+		ID: id,
 	})
+}
+
+func validateCreatePayload(input models.Task) error {
+	if input.Title == "" {
+		return fmt.Errorf("invalid title, expected valid string got %s", input.Title)
+	}
+
+	if input.Description == "" {
+		return fmt.Errorf("invalid description, expected valid string got %s", input.Description)
+	}
+
+	if input.Priority == "" {
+		return fmt.Errorf("invalid priority, expected valid string got %s", input.Priority)
+	}
+
+	if !input.Priority.Valid() {
+		return fmt.Errorf("invalid priority, must be one of %s, got %s", strings.Join(models.ListTaskPriority(), ","), input.Priority)
+	}
+
+	return nil
 }
