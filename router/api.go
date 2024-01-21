736af4a22ec859847bbe1b80b0ced68460122d94
Aman Angira <aman.angira@outlook.com>
2024-01-21 13:43:13 +0530

    Update endpoints in api.go

--- router/api.go
+++ router/api.go
@@ -1,17 +1,27 @@
 package router
 
 import (
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"task/app"
 	"task/app/container"
+	"task/controller"
 	"task/middleware"
 )
 
 func (a api) Init(r *chi.Mux, apiInstance app.IAPI) {
 	r.Use(middleware.ApplyBasicAuthorizer(apiInstance))
 
+	specificTaskPath := fmt.Sprintf("/task/{%s}", controller.TaskIDRequestKey)
 	r.Post("/task", container.APIContainer.TaskController.Create)
-	r.Get("/task", container.APIContainer.TaskController.GetTask)
-	r.Put("/task", container.APIContainer.TaskController.UpdateTask)
-	r.Delete("/task", container.APIContainer.TaskController.DeleteTask)
+	r.Get("/task", container.APIContainer.TaskController.ListTasks)
+	r.Delete(
+		specificTaskPath,
+		container.APIContainer.TaskController.DeleteTask)
+	r.Get(
+		specificTaskPath,
+		container.APIContainer.TaskController.GetTask)
+	r.Patch(
+		specificTaskPath,
+		container.APIContainer.TaskController.UpdateTask)
 }
