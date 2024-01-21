6447b5ccf2d92ffa1059f33e5b220ddf3a738f94
Aman Angira <aman.angira@outlook.com>
2024-01-21 13:41:06 +0530

    Add db tags for task

--- persistence/models/task.go
+++ persistence/models/task.go
@@ -9,11 +9,11 @@ import (
 
 // Task - represents a user task in DB
 type Task struct {
-	ID          string       `json:"id"`
-	Title       string       `json:"title"`
-	Description string       `json:"description"`
-	Priority    TaskPriority `json:"priority"`
-	CreatedAt   time.Time    `json:"created_at"`
-	UpdatedAt   time.Time    `json:"updated_at"`
-	DueAt       time.Time    `json:"due_at"`
+	ID          string       `json:"id" db:"id"`
+	Title       string       `json:"title" db:"title"`
+	Description string       `json:"description" db:"description"`
+	Priority    TaskPriority `json:"priority" db:"priority"`
+	CreatedAt   time.Time    `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time    `json:"updated_at" db:"updated_at"`
+	DueAt       time.Time    `json:"due_at" db:"due_at"`
 }
